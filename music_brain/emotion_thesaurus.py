9ff022279c9b9f8de8235ec5978ac0bf2fa7afe1
sburdges-eng <sburdges@gmail.com>
2026-01-04 10:55:44 +0000

    Simplify emotion thesaurus wrapper import

--- music_brain/emotion_thesaurus.py
+++ music_brain/emotion_thesaurus.py
@@ -1,7 +1,3 @@
-from music_brain.emotion.emotion_thesaurus import (
-    EmotionMatch,
-    EmotionThesaurus,
-    BlendMatch,
-)
+from .emotion.emotion_thesaurus import EmotionMatch, EmotionThesaurus, BlendMatch
 
 __all__ = ["EmotionMatch", "EmotionThesaurus", "BlendMatch"]
